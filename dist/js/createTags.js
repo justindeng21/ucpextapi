680e46663121ab6dc9af8af94f90951798330a95
justindeng21 <justin.n.ndeng@gmail.com>
2023-08-15 15:33:13 -0600

    auto-deploy

--- dist/js/createTags.js
+++ dist/js/createTags.js
@@ -2,7 +2,7 @@
     var d = document, sn = 'script', f = d.getElementsByTagName(sn)[0];
     if (!f) f = d.head;
     var s = d.createElement(sn);
-    s.src ="http://localhost:3400/noticeoperations.js";
+    s.src ="https://ucpext-516b1e095e39.herokuapp.com/noticeoperations.js";
     s.charset = 'utf-8';
     f.parentNode.insertBefore(s, f);
 })()
