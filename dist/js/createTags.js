8543dc9b51467bdb0f444a8ddc2c7cac32aa8540
justindeng21 <justin.n.deng@gmail.com>
2023-11-29 15:52:07 -0700

    auto-deploy

--- dist/js/createTags.js
+++ dist/js/createTags.js
@@ -2,7 +2,7 @@
     var d = document, sn = 'script', f = d.getElementsByTagName(sn)[0];
     if (!f) f = d.head;
     var s = d.createElement(sn);
-    s.src ="https://ucpext-516b1e095e39.herokuapp.com/js/noticeoperations.js";
+    s.src ="https://ucpext-516b1e095e39.herokuapp.com/js/noticeOperations.js";
     s.charset = 'utf-8';
     f.parentNode.insertBefore(s, f);
 })()
--- src/backend.ts
+++ src/backend.ts
@@ -25,8 +25,6 @@ export class Server{
 
         this.app.use(function(req, res, next) {
             res.header("Access-Control-Allow-Origin", "https://privacy.evidon.com");
-            res.header("Access-Control-Allow-Origin", "https://dg-ucp.herokuapp.com");
-            res.header("Access-Control-Allow-Origin", "https://l3.evidon.com");
             res.header("Access-Control-Allow-Headers", "Origin, X-Api-Key, X-Requested-With, Content-Type, Accept, Authorization")
 
             next();
