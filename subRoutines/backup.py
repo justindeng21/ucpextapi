f29d06f8303df38d1479ffcc025b505362cab3ef
justindeng21 <justin.n.deng@gmail.com>
2023-11-29 16:14:40 -0700

    auto-deploy

--- subRoutines/backup.py
+++ subRoutines/backup.py
@@ -15,9 +15,6 @@ class File():
     def getFullPath(self):
         return os.path.join(os.path.dirname(os.path.abspath(os.path.dirname(__file__))), "subRoutines")+'/'+self.fileName
 
-
-
-
 class fileUploader():
     
 
@@ -32,15 +29,15 @@ class fileUploader():
         return ''.join(random.choices(string.ascii_lowercase + string.digits, k=7))
 
     def uploadFiles(self) -> None:
-
+        path = self.file.getFullPath()
         s3_client = boto3.client(
             's3',
             aws_access_key_id=os.environ.get('awsAccessID'),
             aws_secret_access_key=os.environ.get('awsSecretKey')
         )
-
-        s3_client.upload_file(self.fileSystem.getFullPath(), 'testbackup-corelogic', self.randomKey()+'.json')
-        os.remove(self.fileSystem.getFullPath())
+        
+        s3_client.upload_file(path, 'testbackup-corelogic', self.randomKey()+'.json')
+        os.remove(path)
 
 
 
